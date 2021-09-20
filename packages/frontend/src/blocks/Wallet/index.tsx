d8dd831cd65244cef4ca6f776f9d35585cb1657f
Jack <jhburrus3@gmail.com>
2021-09-20 07:48:03 -0600

    able to get token balances from kovan

--- packages/frontend/src/blocks/Wallet/index.tsx
+++ packages/frontend/src/blocks/Wallet/index.tsx
@@ -51,21 +51,24 @@ const Wallet = (props) => {
   // console.log(library)
 
   const fetchTokenBalances = async (account: string, addresses: string[]) => {
-    fetch(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMYAPIKEY}`, {
+    fetch(`https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMYAPIKEY}`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
       },
       body: JSON.stringify({
         jsonrpc: '2.0',
-        method: 'alchemy_getTokenMetadata',
-        params: ['0x1985365e9f78359a9B6AD760e32412f4a445E862'],
-        id: 1,
+        method: 'alchemy_getTokenBalances',
+        params: [
+          '0xE35ef95A80839C3c261197B6c93E5765C9A6a31a',
+          'DEFAULT_TOKENS',
+        ],
+        id: 42,
       }),
     }).then((response) => {
       if (response.ok) {
         response.json().then((json) => {
-          console.log(json)
+          console.log(json.result.tokenBalances)
         })
       }
     })
