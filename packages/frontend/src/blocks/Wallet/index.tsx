32ee6e9437af120f6581e1bbfdb4f5be32d43406
Jack <jhburrus3@gmail.com>
2021-09-20 07:44:18 -0600

    fetch working on mainnet alchemy

--- packages/frontend/src/blocks/Wallet/index.tsx
+++ packages/frontend/src/blocks/Wallet/index.tsx
@@ -51,18 +51,33 @@ const Wallet = (props) => {
   // console.log(library)
 
   const fetchTokenBalances = async (account: string, addresses: string[]) => {
-    const balances = await library.send('eth_getBalance', [
-      '0xE35ef95A80839C3c261197B6c93E5765C9A6a31a',
-      'latest',
-    ])
-    console.log(formatEther(balances))
-    return balances
+    fetch(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMYAPIKEY}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'alchemy_getTokenMetadata',
+        params: ['0x1985365e9f78359a9B6AD760e32412f4a445E862'],
+        id: 1,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((json) => {
+          console.log(json)
+        })
+      }
+    })
+    // console.log(balances)
+    // return balances
     // return balances.tokenBalances.map((balance) => balance.tokenBalance)
   }
 
   useEffect(() => {
     if (library) {
       fetchTokenBalances()
+      // console.log(library)
     }
   }, [])
   // const fetchTokenBalances = async (account: string, addresses: string[]) => {
