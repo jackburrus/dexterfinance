20ef23d3f35330898228321db0885ba0c4afa8d9
Jack <jhburrus3@gmail.com>
2021-09-20 21:04:12 -0600

    Wallet assets looking good

--- packages/frontend/src/blocks/Wallet/index.tsx
+++ packages/frontend/src/blocks/Wallet/index.tsx
@@ -28,25 +28,6 @@ import AssetsPanel from './components/AssetsPanel'
 // import hre, { ethers } from 'hardhat'
 interface Props {}
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  })
-  return response.json() // parses JSON response into native JavaScript objects
-}
-
 const Wallet = (props) => {
   const { provided } = props
   const [walletAmount, setWalletAmount] = useState(null)
@@ -58,12 +39,6 @@ const Wallet = (props) => {
   const [USDValue, setUSDValue] = useState(null)
   const config = useConfig()
   const [activeEthAddress, setActiveEthAddress] = useState(account)
-
-  // useEffect(() => {
-  //   if (tokenBalanceData) {
-  //     console.log(tokenBalanceData)
-  //   }
-  // }, [tokenBalanceData])
 
   return (
     <Box
