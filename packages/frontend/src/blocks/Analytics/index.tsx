cf4fb1b9041dfeeefa34d86fdf243b5095ad7833
Jack <jhburrus3@gmail.com>
2021-09-27 14:51:16 -0600

    box shadow looking better for box

--- packages/frontend/src/blocks/Analytics/index.tsx
+++ packages/frontend/src/blocks/Analytics/index.tsx
@@ -209,9 +209,10 @@ const AnalyticsBlock = ({ provided, uuid }) => {
       margin={'10'}
       justifyContent="flex-start"
       flexDirection="column"
-      bg={colorMode == 'light' ? '#E0EAF2' : '#181C20'}
+      // bg={colorMode == 'light' ? '#E0EAF2' : '#181C20'}
+      bg={colorMode == 'light' ? 'white' : '#181C20'}
       // border={colorMode == 'light' ? '1px solid grey' : null}
-      boxShadow={colorMode == 'light' ? '0 0 5px #333' : null}
+      boxShadow={colorMode == 'light' ? '1px 0 15px 2px #b6bdca' : null}
       borderRadius={'3xl'}
       position="relative"
       p={activeDataType === 'Top Tokens' ? 2 : null}
