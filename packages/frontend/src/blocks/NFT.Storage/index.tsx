2bb0f840f6799ae8e855ac0eb11a9b77bb2cea35
Jack <jhburrus3@gmail.com>
2021-09-30 20:38:41 -0600

    Upload to ipfs finally working

--- packages/frontend/src/blocks/NFT.Storage/index.tsx
+++ packages/frontend/src/blocks/NFT.Storage/index.tsx
@@ -1,46 +1,103 @@
 import { Center, Text } from '@chakra-ui/layout'
 import { CustomBox } from '@components/CustomBox'
-import React, { useEffect } from 'react'
-import { NFTStorage } from 'nft.storage'
+import React, { useEffect, useState } from 'react'
+import { Blob, NFTStorage, File } from 'nft.storage'
 import CloseButton from '@components/CloseButton'
-import Image from 'next/image'
-
-const client = new NFTStorage({ token: process.env.NFTStorage })
+import { Dropzone, FileItem } from 'dropzone-ui'
 ;('../../../public/images/logo-metamask.png')
-const fetchData = async () => {
-  // console.log(client)
-  const metadata = await client.store({
-    name: 'Pinpie',
-    description: 'Pin is not delicious beef!',
-    image: new File(
-      ['../../../public/images/logo-metamask.png'],
-      'logometamask.png',
-      {
-        type: 'image/png',
-      }
-    ),
-  })
-  console.log('metadata', metadata)
-}
+const client = new NFTStorage({ token: process.env.NFTStorage })
 
 const NFT_Storage = (props) => {
   const { uuid } = props
+  const [files, setFiles] = useState([])
+  const [uploadedFile, setUploadedFile] = useState([])
+
+  const [title, setTitle] = useState('My NFT')
+  const [description, setDescription] = useState('')
+  const updateFiles = (incommingFiles) => {
+    // console.log(typeof incommingFiles[0])
+
+    console.log(incommingFiles[0])
+    setFiles(incommingFiles)
+  }
+
+  const storeFileAsBlob = async (file) => {
+    const cid =
+      client !== undefined &&
+      (await client.storeBlob(new Blob([Array.isArray(file) ? file[0] : file])))
+    return cid
+  }
+
+  const fetchData = async (file) => {
+    // console.log(client)
+    // const metadata = await client.store({
+    //   name: 'Pinpie',
+    //   description: 'Pin is not delicious beef!',
+    //   image: new File(
+    //     [['../../../public/images/logo-metamask.png'],
+    //     'logometamask.png',
+    //     {
+    //       type: 'image/png',
+    //     }
+    //   ),
+    // })
+
+    const metadata = await client.store({
+      name: 'NFT',
+      description: 'My NFT File!',
+      image: new File([file], 'mynft.jpeg', { type: 'image/jpeg' }),
+    })
+    console.log('metadata', metadata)
+  }
+
   useEffect(() => {
-    fetchData()
-  }, [])
+    // console.log(files[0]['file'])
+    if (files.length > 0) {
+      fetchData(files[0]['file'])
+    }
+  }, [files])
+
   return (
     <CustomBox>
       <CloseButton blockID={uuid} />
       <Center>
         <Text color={'white'}>Storage</Text>
-        <Image
-          src={'/images/logo-metamask.png'}
+        {/* <Image
+          src={'/images/pinpie.jpeg'}
           layout={'fill'}
           objectFit="contain"
-        />
+        /> */}
+        <Dropzone onChange={updateFiles} value={files}>
+          {files.map((file) => (
+            <FileItem {...file} preview />
+          ))}
+        </Dropzone>
       </Center>
     </CustomBox>
   )
 }
 
 export default NFT_Storage
+
+// const onUpload = async file => {
+//     if (file) {
+//       setUploadedFile(new File([await file.arrayBuffer()], file.name, { type: file.type }));
+//     }
+//     return false;
+//   };
+//   const updateFiles = (incommingFiles) => {
+//     console.log(incommingFiles[0]['file']['name'])
+// onUpload(incommingFiles[0]['file'])
+//     setFiles(incommingFiles)
+//   }
+//   const { uuid } = props
+//   useEffect(() => {
+//     fetchData(files[0]['file']['name'])
+//   }, [files])
+
+//   const onUpload = async file => {
+//     if (file) {
+//       setUploadedFile(new File([await file.arrayBuffer()], file.name, { type: file.type }));
+//     }
+//     return false;
+//   };
