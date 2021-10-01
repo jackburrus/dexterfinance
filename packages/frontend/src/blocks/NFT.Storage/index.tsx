b68847b31aec1ed8b500cbfc9421da1d3b82925c
Jack <jhburrus3@gmail.com>
2021-10-01 17:57:59 -0600

    Metadata showing on screen working

--- packages/frontend/src/blocks/NFT.Storage/index.tsx
+++ packages/frontend/src/blocks/NFT.Storage/index.tsx
@@ -1,95 +1,107 @@
-import { Center, Text } from '@chakra-ui/layout'
+import { Box, Center, Text } from '@chakra-ui/layout'
 import { CustomBox } from '@components/CustomBox'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Blob, NFTStorage, File } from 'nft.storage'
 import CloseButton from '@components/CloseButton'
 import { Dropzone, FileItem } from 'dropzone-ui'
-;('../../../public/images/logo-metamask.png')
+import { useDropzone } from 'react-dropzone'
+
 const client = new NFTStorage({ token: process.env.NFTStorage })
 
 const NFT_Storage = (props) => {
   const { uuid } = props
   const [files, setFiles] = useState([])
-  const [uploadedFile, setUploadedFile] = useState([])
+  const [ipfsFiles, setIpfsFiles] = useState([])
+  // const [uploadedFile, setUploadedFile] = useState([])
 
   const [title, setTitle] = useState('My NFT')
-  const [description, setDescription] = useState('')
-  const updateFiles = (incommingFiles) => {
-    // console.log(typeof incommingFiles[0])
+  const [description, setDescription] = useState([])
 
-    console.log(incommingFiles[0])
-    setFiles(incommingFiles)
-  }
-
-  const storeFileAsBlob = async (file) => {
-    const cid =
-      client !== undefined &&
-      (await client.storeBlob(new Blob([Array.isArray(file) ? file[0] : file])))
-    return cid
-  }
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log(acceptedFiles)
+    setFiles(acceptedFiles)
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   const fetchData = async (file) => {
-    // console.log(client)
-    // const metadata = await client.store({
-    //   name: 'Pinpie',
-    //   description: 'Pin is not delicious beef!',
-    //   image: new File(
-    //     [['../../../public/images/logo-metamask.png'],
-    //     'logometamask.png',
-    //     {
-    //       type: 'image/png',
-    //     }
-    //   ),
-    // })
-
     const metadata = await client.store({
       name: 'NFT',
       description: 'My NFT File!',
       image: new File([file], 'mynft.jpeg', { type: 'image/jpeg' }),
     })
+    setIpfsFiles((oldArray) => [...oldArray, metadata])
     console.log('metadata', metadata)
   }
 
   useEffect(() => {
     // console.log(files[0]['file'])
     if (files.length > 0) {
-      fetchData(files[0]['file'])
+      files.map((file) => {
+        fetchData(file)
+      })
     }
   }, [files])
 
   return (
     <CustomBox>
       <CloseButton blockID={uuid} />
-      <Center>
-        {/* <Text color={'white'}>Storage</Text> */}
-        {/* <Image
-          src={'/images/pinpie.jpeg'}
-          layout={'fill'}
-          objectFit="contain"
-        /> */}
-        <Dropzone
-          backgroundColor={'#181C20'}
-          color={'white'}
-          style={{ borderRadius: '10px' }}
-          style={{
-            backgroundImage: `url(${"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='black' stroke-width='2' stroke-dasharray='15%2c 19' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e"})`,
-          }}
-          view={'list'}
-          // header={false}
-          footer={false}
-          onChange={updateFiles}
-          value={files}
-        >
-          {files.map((file) => (
-            <FileItem {...file} preview />
-          ))}
-        </Dropzone>
-      </Center>
+      <div
+        {...getRootProps()}
+        style={{
+          border: '1px dotted white',
+          height: '200px',
+        }}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      {ipfsFiles.length > 0 && (
+        <Box>
+          <Text>{ipfsFiles[0].url}</Text>
+        </Box>
+      )}
     </CustomBox>
   )
 }
 
 export default NFT_Storage
+
+// const updateFiles = (incommingFiles) => {
+//   // console.log(typeof incommingFiles[0])
+
+//   console.log(incommingFiles)
+//   setFiles(incommingFiles)
+// }
+// <Center>
+//         {/* <Text color={'white'}>Storage</Text> */}
+//         {/* <Image
+//           src={'/images/pinpie.jpeg'}
+//           layout={'fill'}
+//           objectFit="contain"
+//         /> */}
+//         <Dropzone
+//           backgroundColor={'#181C20'}
+//           color={'white'}
+//           style={{ borderRadius: '10px' }}
+//           style={{
+//             backgroundImage: `url(${"data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='black' stroke-width='2' stroke-dasharray='15%2c 19' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e"})`,
+//           }}
+//           view={'list'}
+//           // header={false}
+//           footer={false}
+//           onChange={updateFiles}
+//           value={files}
+//         >
+//           {files.map((file) => (
+//             <FileItem {...file} preview />
+//           ))}
+//         </Dropzone>
+//       </Center>
 
 // const onUpload = async file => {
 //     if (file) {
